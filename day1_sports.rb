452341e4c9a935c544385f78c401216ff88ac660
Stuart Black <stuartblack@gmail.com>
2018-07-02 22:12:49 +0100

    Part A and Part B completed

--- day1_sports.rb
+++ day1_sports.rb
@@ -1,17 +1,27 @@
 class Team
-attr_reader :team_name, :player, :coach, :team
-attr_writer :team_name, :player, :coach, :team
+  attr_reader :team_name, :player, :coach, :team, :points
+  attr_writer :team_name, :player, :coach, :team, :points
 
- def initialize (team_name, name, coach, team)
-   @name = name
-   @player = player
-   @coach = coach
-   @team = team
- end
+  def initialize (team_name, name, coach, team, points)
+    @name = name
+    @player = player
+    @coach = coach
+    @team = team
+    @points = points
+  end
 
-def add_new_player
-  @team << "Chris"
-return @team.length
-end
+  def add_new_player(new_man)
+    @team << new_man
+  end
+
+  def in_team?(player_name)
+    plays_in_team =  @team.include? player_name
+    return plays_in_team
+  end
+
+  def calculate_points(result)
+    @points += 3 if result == "win"
+    @points += 0 if result == "lose"
+  end
 
 end
--- specs/day1_sports_spec.rb
+++ specs/day1_sports_spec.rb
@@ -4,18 +4,36 @@ require('minitest/rg')
 
 class TestTeam < MiniTest::Test
 
-def test_add_new_player
-no1 = Team.new("Ruby Duckies", "Jardine", "Mr. Head Coach",[])
-no1.add_new_player
-assert_equal(1, @team.length)
+  def test_add_new_player
+    no1 = Team.new("Ruby Duckies", "Jardine", "Mr. Head Coach",[], 0)
+    no1.add_new_player("Dave")
+    team_size = no1.team.length
+    assert_equal(1, team_size)
+  end
+
+def test_in_team?
+  no1 = Team.new("Ruby Duckies", "Jardine", "Mr. Head Coach",[], 0)
+  no1.add_new_player("Dave")
+  check = no1.in_team?("Dave")
+  assert_equal(true, check)
 end
 
+def test_calculate_points
+  no1 = Team.new("Ruby Duckies", "Jardine", "Mr. Head Coach",[], 0)
 
+  # check for win
+  no1.points = 0
+  no1.calculate_points("win")
+  check = no1.points
+  p no1.points
+  assert_equal(3, check)
 
-
-
-
-
-
-
+  # check for no win
+  no1.points = 0 # Reset
+  no1.calculate_points("lose")
+  check = no1.points
+  p no1.points
+  assert_equal(0, check)
 end
+
+end #class end
